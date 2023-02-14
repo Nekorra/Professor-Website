a174aaf294c30d7d8577855d53f2da7632b3fea4
Nekorra <abhisrin@gmail.com>
2023-02-13 16:46:38 -0800

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslationLoaderService } from '../service/translation-loader.service';
+import { locale as english } from '../shared/i18n/en';
+import { locale as french } from '../shared/i18n/fr';
+import Typed from 'typed.js';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private _translationLoaderService: TranslationLoaderService) {
+    this._translationLoaderService.loadTranslations(english, french);
+  }
+  ngOnInit(): void {
+    var options = {
+      strings: ['Professor Homayoun.', 'a Researcher.', 'an Innovator.', 'a Teacher.',],
+      typeSpeed: 80,
+      backSpeed: 50,
+      loop: true,
+    };
+    
+    var typed = new Typed('.typed', options);
+    typed.reset(true)
+  }
+}
