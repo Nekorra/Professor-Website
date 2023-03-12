145972b6834e8cd0a483d5fd5bb36d84acf88f9f
Abhinav Srinivas <abhisrin@Abhis-MBP-2.attlocal.net>
2023-03-12 12:41:22 -0700

    linked research to backend

--- src/app/research/research.component.ts
+++ src/app/research/research.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RestapiService } from '../services/restapi.service';
 @Component({
   selector: 'app-research',
   templateUrl: './research.component.html',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResearchComponent implements OnInit {
 
-  constructor() { }
+  researchData: any[] = []
+  constructor(
+    private RestapiService: RestapiService,
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    console.log("here");
+    this.RestapiService.getData("https://Nekorra.github.io/prof-data/research.json").subscribe((res) => {
+      console.log(res);
+      this.researchData = res["research"];
+      console.log(this.researchData);
+    })
+
+  
   }
 
 }
